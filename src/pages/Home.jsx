286f3398a7042bbd46f04068fb749aacfb0d325e
Sandeep Kumar Bhaviri <bsandeep@corp.untd.com>
2023-10-16 15:16:03 +0530

    convert-to-solid

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import './css/Home.css'
+
+export default function Home() {
+    return (
+        <div className='a-center h-screen w-screen'>
+            <h1 className='p-5 text-xl'>Solid Home!
+            </h1>
+            <button className='btn'>Click</button>
+        </div>
+    )
+}
